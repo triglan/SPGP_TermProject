2178f17c2d6ee6dadbde00bbd51fb98c779f1953
triglan <121035322+triglan@users.noreply.github.com>
2025-05-06 15:15:05 +0900

    배경 및 애니메이션 추가 확보

--- DeadlockDuel/app/src/main/java/com/example/deadlockduel/app/DeadlockDuel.java
+++ DeadlockDuel/app/src/main/java/com/example/deadlockduel/app/DeadlockDuel.java
@@ -9,8 +9,6 @@ public class DeadlockDuel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // 다음 단계에서 만들 TileMapView 를 붙일 거야
         TileMapView mapView = new TileMapView(this);
         setContentView(mapView);
     }
