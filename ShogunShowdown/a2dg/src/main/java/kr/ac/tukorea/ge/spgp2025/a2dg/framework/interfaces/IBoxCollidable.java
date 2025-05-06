e8edb104da1a130442b7bcd7331a4860463f8edd
triglan <121035322+triglan@users.noreply.github.com>
2025-05-06 11:39:03 +0900

    메인화면 글자와 버튼

--- ShogunShowdown/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/interfaces/IBoxCollidable.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces;
-
-import android.graphics.RectF;
-
-public interface IBoxCollidable {
-    public RectF getCollisionRect();
-}
--- ShogunShowdown/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2025/a2dg/framework/interfaces/IRecyclable.java
+++
@@ -1,5 +0,0 @@
-package kr.ac.tukorea.ge.spgp2025.a2dg.framework.interfaces;
-
-public interface IRecyclable {
-    public void onRecycle();
-}
